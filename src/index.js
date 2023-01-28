c8b3350bf5a12f6bcba27511650663b76117f6ab
Arina Gladysheva <arina1110@mail.ru>
2023-01-28 17:57:21 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let beginBr = [];
+  let pairBr = {};
+  let temporary = [];
+  bracketsConfig.map(element => {
+    beginBr.push(element[0]);
+    pairBr[element[1]] = element[0];
+  });
+
+  for (let i = 0; i < str.length; i++) {
+    let current = str[i];
+    if (beginBr.includes(current)) {
+      if (temporary.length === 0) {
+        temporary.push(current);
+      } else if (temporary.length !== 0 && pairBr[current] === current) {
+        if (temporary[temporary.length - 1] === current) {
+          temporary.pop();
+        } else if (temporary[temporary.length - 1] !== current) {
+          temporary.push(current);
+        }
+      } else if (temporary.length !== 0 && pairBr[current] !== current) {
+        temporary.push(current);
+      }
+    } else {
+      if (temporary.length === 0) {
+        return false;
+      } else if (temporary.length !== 0 && pairBr[current] === temporary[temporary.length - 1]) {
+        temporary.pop();
+      } else if (temporary.length !== 0 && pairBr[current] !== temporary[temporary.length - 1]) {
+        return false;
+      }
+    }
+  }
+  
+    return temporary.length === 0;
+  
+};
+
+
